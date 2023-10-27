a25f6d53123b5469281d430b0c3325ca34ae14cf
Mourad EL MOUFID <elmoufid.mourad@gmail.com>
2023-10-27 13:47:00 +0200

    Managed Error Handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -36,6 +37,8 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("https://localhost:4200"));
 
---
+++ client/src/app/errors/test-error/test-error.component.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.css']
+})
+export class TestErrorComponent implements OnInit {
+  BaseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
+
+  constructor(private http:HttpClient, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error(){
+    this.http.get(this.BaseUrl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: error => {
+        console.log(error);
+      }
+    })
+  }
+
+  get400Error(){
+    this.http.get(this.BaseUrl + 'buggy/bad-request').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get401Error(){
+    this.http.get(this.BaseUrl + 'buggy/auth').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400ValidationError(){
+    this.http.post(this.BaseUrl + 'account/register', {}).subscribe({
+      next: response => console.log(response),
+      error: error => {
+        console.log(error);
+        this.validationErrors = error;
+      }
+    })
+  }
+}
