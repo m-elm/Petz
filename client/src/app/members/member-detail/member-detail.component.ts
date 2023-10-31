982dd1fa140551fe978a67165633b22c34b05408
Mourad EL MOUFID <elmoufid.mourad@gmail.com>
2023-10-31 17:12:09 +0100

    Added members and details pages

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,10 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Gallery, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule],
 })
 export class MemberDetailComponent {
+  member: Member | undefined;
+  images: GalleryItem[] = [];
 
+  constructor(
+    private memberService: MembersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    // this function loads the members
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+
+    this.memberService.getMember(username).subscribe({
+      next: (member) => {
+        (this.member = member), this.getImages();
+      },
+    });
+  }
+
+  getImages() {
+    // this function is loads the images of each member
+    if (!this.member) return;
+    for (const photo of this.member?.photos) {
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+    }
+  }
 }
